0b58a469fdd1862e2e7f94802c0bf8454bd6dd64
saurabhjn76 <saurabhjn76@gmail.com>
2016-07-05 01:11:56 +0530

    update

---
+++ app/src/main/java/com/orca/dotz/customviews/VerticalViewPager.java
@@ -0,0 +1,7 @@
+package com.orca.dotz.customviews;
+
+/**
+ * Created by saurabh on 4/7/16.
+ */
+public class VerticalViewPager {
+}
---
+++ app/src/main/java/com/orca/dotz/utils/DirectionsJSONParser.java
@@ -0,0 +1,7 @@
+package com.orca.dotz.utils;
+
+/**
+ * Created by saurabh on 4/7/16.
+ */
+public class DirectionsJSONParser {
+}
