1a43b4f261ca563e8f4f16ac7159b59db6989f91
saurabhjn76 <saurabhjn76@gmail.com>
2016-07-11 20:52:48 +0530

    added notifications

---
+++ app/src/main/java/com/orca/dotz/AlarmReciever.java
@@ -0,0 +1,7 @@
+package com.orca.dotz;
+
+/**
+ * Created by saurabh on 11/7/16.
+ */
+public class AlarmReciever {
+}
---
+++ app/src/main/java/com/orca/dotz/SalonProfile.java
@@ -0,0 +1,13 @@
+package com.orca.dotz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SalonProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salon_profile);
+    }
+}
